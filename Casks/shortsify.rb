19ae687fa50fef562e71149d61a29e9c3c72e637
Aya <aya@Ayas-MacBook-Air.local>
2026-07-08 21:26:28 +0200

    Add Shortsify cask

---
+++ Casks/shortsify.rb
@@ -0,0 +1,11 @@
+cask "shortsify" do
+  version "1.0.3"
+  sha256 "cb93cf7f593172b556449efb838712b34096c8803573c9268763010ea5440754"
+
+  url "https://github.com/yooyplay/Shortsify/releases/download/v#{version}/Shortsify.dmg"
+  name "Shortsify"
+  desc "Convert any video to YouTube Shorts, TikTok, and Reels format"
+  homepage "https://yooyplay.github.io/Shortsify/"
+
+  app "Shortsify.app"
+end
