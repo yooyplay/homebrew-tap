5eae2595eda0f7c22c043466ddf51ad5d0642c26
Aya <aya@Ayas-MacBook-Air.local>
2026-07-11 14:07:24 +0200

    Update Shortsify to v1.0.6

--- Casks/shortsify.rb
+++ Casks/shortsify.rb
@@ -1,6 +1,6 @@
 cask "shortsify" do
-  version "1.0.5"
-  sha256 "3c11259ea7394ab890c746c4677c14f4f81e65585b4a8a386a7cc9c97341591b"
+  version "1.0.6"
+  sha256 "318488f57b8114fc4c6b89883aaac58f81d6ddf90e3e92354225daa310a2d18c"
 
   url "https://github.com/yooyplay/Shortsify/releases/download/v#{version}/Shortsify.dmg"
   name "Shortsify"
